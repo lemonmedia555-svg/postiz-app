76137a2b07171d14f70c74f72dbd0d5410a8b296
Ivan Tymak <lemonmedia555@gmail.com>
2026-09-22 13:07:27 +0300

    Уточнить права YouTube для публикации и аналитики

--- libraries/nestjs-libraries/src/integrations/social/youtube.provider.ts
+++ libraries/nestjs-libraries/src/integrations/social/youtube.provider.ts
@@ -60,12 +60,8 @@ export class YoutubeProvider extends SocialAbstract implements SocialProvider {
   dto = YoutubeSettingsDto;
   scopes = [
     'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/youtube',
-    'https://www.googleapis.com/auth/youtube.force-ssl',
     'https://www.googleapis.com/auth/youtube.readonly',
     'https://www.googleapis.com/auth/youtube.upload',
-    'https://www.googleapis.com/auth/youtubepartner',
     'https://www.googleapis.com/auth/yt-analytics.readonly',
   ];
 
